98ab3450a79b6629f67bdfeaec4d980f71b9a127
LucaGambera <123939570+LucaGambera@users.noreply.github.com>
2023-03-31 16:14:13 +0200

    le get api fonctionne mais mal encodé

--- Code/Luca/Service_Rest/src/java/rest/Message.java
+++ Code/Luca/Service_Rest/src/java/rest/Message.java
@@ -12,6 +12,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -24,17 +25,17 @@ public class Message {
 
     private static final String API_KEY = "api_key=eb482103d8c607c29a702217d8ad491a";
     private static final String BASE_URL = "https://api.themoviedb.org/3";
-    private static final String API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&"+ API_KEY +"&language=fr-FR";
+    private static final String API_URL = BASE_URL + "/movie/top_rated?" + API_KEY + "&language=fr-FR&page=1";
 
     /**
-     * const API_KEY = 'api_key=eb482103d8c607c29a702217d8ad491a';
-     * const BASE_URL = 'https://api.themoviedb.org/3';
-     * const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY + '&language=fr-FR';
-     
+     * const API_KEY = 'api_key=eb482103d8c607c29a702217d8ad491a'; const
+     * BASE_URL = 'https://api.themoviedb.org/3'; const API_URL = BASE_URL +
+     * '/discover/movie?sort_by=popularity.desc&' + API_KEY + '&language=fr-FR';
+     *
      */
     @Context
     private UriInfo context;
-
+    private WebTarget webTarget;
     private Client client;
 
     /**
@@ -42,6 +43,8 @@ public class Message {
      * Creates a new instance of Message
      */
     public Message() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(API_URL);
 
     }
 
@@ -70,11 +73,21 @@ public class Message {
     @GET
     @Path("getFilm")
     @Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
-    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public String getMessage() {
         String retourApi = "Test";
+        WebTarget ressource = webTarget;
+        retourApi = ressource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).header("Accept-Charset", "UTF-8").get(String.class);
 
         return retourApi;
+    }
+
+    @GET
+    @Path("getAuthor")
+    @Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)
+    public String getAuthor() {
+        return "Fait par Gambera Luca !";
     }
 
 }
