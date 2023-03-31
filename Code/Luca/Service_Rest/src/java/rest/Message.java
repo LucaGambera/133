508bcf67ea1199e9480b79c2ce2c37f081f42fbe
LucaGambera <123939570+LucaGambera@users.noreply.github.com>
2023-03-31 14:49:32 +0200

    Création rest

---
+++ Code/Luca/Service_Rest/src/java/rest/Message.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author gamberal01
+ */
+@Path("Service2")
+public class Message {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of Message
+     */
+    public Message() {
+    }
+
+    /**
+     * Retrieves representation of an instance of rest.Message
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of Message
+     *
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+
+    @GET
+    @Path("getFilm")
+    @Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)
+    public String getMessage(){
+        String retourApi = "Test";
+        
+        return retourApi;
+    }
+
+}
