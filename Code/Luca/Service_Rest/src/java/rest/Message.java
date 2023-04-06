b95c7d5b500eaea6919ec4da2dbc189cfc0ce4ed
LucaGambera <123939570+LucaGambera@users.noreply.github.com>
2023-04-06 14:56:11 +0200

    Rest finit

--- Code/Luca/Service_Rest/src/java/rest/Message.java
+++ Code/Luca/Service_Rest/src/java/rest/Message.java
@@ -9,7 +9,9 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
+import com.google.gson.Gson;
 import javax.ws.rs.client.WebTarget;
 
 /**
@@ -20,9 +22,10 @@ import javax.ws.rs.client.WebTarget;
 @Path("Service2")
 public class Message {
 
-    private static final String API_KEY = "api_key=eb482103d8c607c29a702217d8ad491a";
-    private static final String BASE_URL = "https://api.themoviedb.org/3";
-    private static final String API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY + "&language=fr-FR&page=1";
+    private static final String API_KEY_LANGUE = "api_key=eb482103d8c607c29a702217d8ad491a&language=fr-FR";
+
+    private static final String API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&" + API_KEY_LANGUE + "&page=";
+    private static final String API_CHERCHER_FILM = "https://api.themoviedb.org/3/movie/";
 
     /**
      * const API_KEY = 'api_key=eb482103d8c607c29a702217d8ad491a'; const
@@ -32,7 +35,8 @@ public class Message {
      */
     @Context
     private UriInfo context;
-    private WebTarget webTarget;
+    private WebTarget films;
+    private WebTarget film;
     private Client client;
 
     /**
@@ -41,7 +45,7 @@ public class Message {
      */
     public Message() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
-        webTarget = client.target(API_URL);
+
     }
 
     /**
@@ -55,17 +59,29 @@ public class Message {
     /**
      * Retrieves representation of an instance of rest.Message
      *
+     * @param id
      * @return an instance of java.lang.String
      */
     @GET
     @Path("getFilms")
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-    public String getFilms() {
+    public String getFilms(@QueryParam("PAGE") String page) {
         String retourApi = "Test";
-        WebTarget ressource = webTarget;
+        films = client.target(API_URL + page);
+        WebTarget ressource = films;
         retourApi = ressource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).header("Accept-Charset", "UTF-8").get(String.class);
         return retourApi;
     }
 
+    @GET
+    @Path("getFilm")
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public String getFilm(@QueryParam("ID") String id) {
+        String retourApi = "Test";
+        film = client.target(API_CHERCHER_FILM + id + "?"+ API_KEY_LANGUE);
+        WebTarget ressource = film;
+        retourApi = ressource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).header("Accept-Charset", "UTF-8").get(String.class);
+        return retourApi;
+    }
 
 }
