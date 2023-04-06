f93892a6bef5b6653150e80f944f7d7657b5a503
MatteoLeonetti <123940578+MatteoLeonetti@users.noreply.github.com>
2023-04-06 15:38:23 +0200

    liredb

--- Code/Matteo/Service_Rest/src/java/rest/Message.java
+++ Code/Matteo/Service_Rest/src/java/rest/Message.java
@@ -4,6 +4,7 @@
  */
 package rest;
 
+import com.google.gson.Gson;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -11,7 +12,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import workers.WrkDB;
 
 /**
  * REST Web Service
@@ -23,22 +26,45 @@ public class Message {
 
     @Context
     private UriInfo context;
+    private WrkDB wrkdb;
 
     /**
      * Creates a new instance of Message
      */
     public Message() {
+        wrkdb = new WrkDB("3306", "mydb");
     }
 
     /**
      * Retrieves representation of an instance of rest.Message
+     *
      * @return an instance of java.lang.String
      */
     @GET
+    @Path("getAvis")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAvis() {
+        Gson gson = new Gson();
+        String json;
+        try {
+            json = gson.toJson(wrkdb.getAvis());
+        } catch (Exception e) {
+            json = gson.toJson("error");
+        }
+        return json;
+    }
+
+    @GET
     @Path("getUser")
-    @Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
-    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)
-    public String getMessage() {
-        return "Bonjour tout le monde !";
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(@QueryParam("PK") int pk) {
+        Gson gson = new Gson();
+        String json;
+        try {
+            json = gson.toJson(wrkdb.getUser(pk));
+        } catch (Exception e) {
+            json = gson.toJson("error");
+        }
+        return json;
     }
 }
--- Code/Matteo/Service_Rest/src/java/workers/WrkDB.java
+++ Code/Matteo/Service_Rest/src/java/workers/WrkDB.java
@@ -10,54 +10,57 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 /**
  *
  * @author BuetschiD
  */
 public class WrkDB {
+
     private String port;
     private String dbName;
-    
-    public WrkDB(String port, String dbName){
+
+    public WrkDB(String port, String dbName) {
         this.port = port;
         this.dbName = dbName;
     }
-    
-     /**
-     * Ouverture d'une connexion sur la base de données.
-     * Ajouter mysql-connector-j-....jar dans les librairies.
-     * 
+
+    /**
+     * Ouverture d'une connexion sur la base de données. Ajouter
+     * mysql-connector-j-....jar dans les librairies.
+     *
      * @return true si tout s'est bien passé, sinon false
      */
-     private Connection dbConnexion;
-    
-    public boolean openConnexion(){
+    private Connection dbConnexion;
+
+    public boolean openConnexion() {
 
         final String url = "jdbc:mysql://localhost:" + port + "/" + dbName + "?serverTimezone=CET";
         final String user = "root";
-        final String pw =  "emf123";// 
+        final String pw = "emf123";
         boolean result = false;
- 
-        try{
+
+        try {
             //nécessaire pour fonctionnement en web
             Class.forName("com.mysql.jdbc.Driver");
-        } catch ( ClassNotFoundException ex ) {
-             System.out.println("Connexion au driver JDBC à échoué!\n" + ex.getMessage());
-        } 
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Connexion au driver JDBC à échoué!\n" + ex.getMessage());
+        }
         try {
-            dbConnexion = DriverManager.getConnection( url, user, pw );
+            dbConnexion = DriverManager.getConnection(url, user, pw);
             //System.out.println("Connection successfull");
             result = true;
 
-        } catch ( SQLException ex ) {
-             System.out.println("Connexion à la BD a échouée!\n" + ex.getMessage());
-        }     
+        } catch (SQLException ex) {
+            System.out.println("Connexion à la BD a échouée!\n" + ex.getMessage());
+        }
         return result;
     }
-    
-     /**
+
+    /**
      * Cette méthode permet de fermer la connexion à la base de données après
      * que la requête ait été envoyée.
+     *
      * @return true si tout s'est bien passé, sinon false
      */
     public boolean closeConnexion() {
@@ -76,7 +79,39 @@ public class WrkDB {
         return result;
     }
 
-    public ArrayList<String> getUser() {
+    public ArrayList<String> getAvis() {
+        ArrayList<String> lstAvis = null;
+        boolean result = openConnexion();
+        if (result) {
+            System.out.println("connection ok");
+            PreparedStatement ps = null;
+            String avis = "";
+            lstAvis = new ArrayList<String>();
+            try {
+                ps = dbConnexion.prepareStatement("SELECT * FROM t_avis");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    avis = (String) rs.getString(1);
+                    avis += "," + (String) rs.getString(2);
+                    avis += "," + (String) rs.getString(3);
+                    avis += "," + (String) rs.getString(4);
+                    lstAvis.add(avis);
+                }
+                rs.close();
+                result = true;
+                System.out.println("OK");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            if (result) {
+                result = closeConnexion();
+            }
+        }
+        return lstAvis;
+    }
+
+    public ArrayList<String> getUser(int PK) {
         ArrayList<String> lstUser = null;
         boolean result = openConnexion();
         if (result) {
@@ -85,10 +120,13 @@ public class WrkDB {
             String user = "";
             lstUser = new ArrayList<String>();
             try {
-                ps = dbConnexion.prepareStatement("SELECT Nom FROM t_users");
+                ps = dbConnexion.prepareStatement("SELECT * FROM mydb.t_users where PK_Users = " + PK + ";");
                 ResultSet rs = ps.executeQuery();
                 while (rs.next()) {
                     user = (String) rs.getString(1);
+                    user += "," + (String) rs.getString(2);
+                    user += "," + (String) rs.getString(3);
+
                     lstUser.add(user);
                 }
                 rs.close();
